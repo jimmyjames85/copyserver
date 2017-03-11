c6a7d0e6df24781c1aa60fe2733c25b3b719694f
jimmyjames <james.tappe@sendgrid.com>
2017-03-10 20:33:46 -0800

    save on a cron, can't add empty items, web redirection

--- copyserver.go
+++ copyserver.go
@@ -2,23 +2,26 @@ package copyserver
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 type copyserver struct {
-	host      string
-	port      int
-	apikey    string
-	savefile  string
-	todolists Todos
+	host             string
+	port             int
+	apikey           string
+	savefile         string
+	todolists        Todos
 }
 
 func NewCopyserver(port int, host, apikey, savefile string) *copyserver {
 	c := &copyserver{
-		host:   host,
-		port:   port,
-		apikey: apikey,
-		savefile: savefile,
+		host:      host,
+		port:      port,
+		apikey:    apikey,
+		savefile:  savefile,
 		todolists: NewTodos(),
 	}
 	return c
@@ -28,18 +31,38 @@ func NewCopyserver(port int, host, apikey, savefile string) *copyserver {
 func (c *copyserver) Serve() error {
 
 	http.HandleFunc("/todo/web/add", c.handleWebAdd)
-	//http.HandleFunc("/todo/web/get", todoWebGet)
+	http.HandleFunc("/todo/web/add_redirect", c.handleWebAddWithRedirect)
+	http.HandleFunc("/todo/web/remove_redirect", c.handleWebRemoveWithRedirect)
 	http.HandleFunc("/todo/web/getall", c.handleWebGetAll)
-
+	http.HandleFunc("/test", c.handleTest)
 	http.HandleFunc("/todo/add", c.handleListAdd)
 	http.HandleFunc("/todo/get", c.handleListGet)
 	http.HandleFunc("/todo/getall", c.handleListGetAll)
 	http.HandleFunc("/todo/getindexed", c.handleListGetIndexed)
 	http.HandleFunc("/todo/remove", c.handleListRemove)
 	http.HandleFunc("/todo/setindex", c.handleListSetIndex)
-
-	http.HandleFunc("/todo/save", c.handleSaveListsToDisk) //todo put this on a cron
+	http.HandleFunc("/todo/save", c.handleSaveListsToDisk) //todo save on every modification
 	http.HandleFunc("/todo/load", c.handleLoadListsFromDisk)
+	http.HandleFunc("/healthcheck", c.handleHealthcheck)
 
+	if _, err := os.Stat(c.savefile); err == nil {
+		err := c.todolists.LoadFromDisk(c.savefile)
+		if err != nil {
+			log.Fatalf("unable to load from previous file: %s\n", c.savefile)
+		}
+	}
+
+	//todo save on every modification
+	// save on a cron
+	go func() {
+		saveTimer := time.Tick(60 * time.Second) //todo set an env var
+		for _ = range saveTimer {
+			err := c.todolists.SavetoDisk(c.savefile)
+			if err != nil {
+				fmt.Printf(outcomeMessage(false, fmt.Sprintf("%s", err))) //todo notify
+				return
+			}
+		}
+	}()
 	return http.ListenAndServe(fmt.Sprintf(":%d", c.port), nil)
 }
--- handler.go
+++ handler.go
@@ -8,26 +8,26 @@ import (
 )
 
 const defaultlist = "main"
-//var saveFileloc = "/tmp/todolists.json"
-//var theapikey = "something"
-//var host="localhost"
-//var port=12345
 
-//func SetHost(hostname string){
-//	host=hostname
-//}
-//
-//func SetPort(p int){
-//	port=p
-//}
-//
-//func SetSaveFileloc(fileloc string){
-//	saveFileloc = fileloc
-//}
-//
-//func SetApikey(apikey string){
-//	theapikey = apikey
-//}
+func (c *copyserver)handleTest(w http.ResponseWriter, r *http.Request) {
+	if !handleParseFormData(w, r) {
+		return
+	}
+	for k, v := range r.Form{
+		io.WriteString(w, fmt.Sprintf("parm=%s\n",k))
+		for _, val := range v{
+			io.WriteString(w, fmt.Sprintf("\t%s\n",val))
+		}
+	}
+}
+
+func (c *copyserver)handleHealthcheck(w http.ResponseWriter, r *http.Request) {
+	if !handleParseFormData(w, r) {
+		return
+	}
+	resp := `{"ok":true,"endpoints":["/healthcheck","/test","/todo/add","/todo/get","/todo/getall","/todo/getindexed","/todo/load","/todo/remove","/todo/save","/todo/setindex","/todo/web/add","/todo/web/getall"]}`
+	io.WriteString(w, resp)
+}
 
 // e.g.
 //
@@ -50,9 +50,13 @@ func (c *copyserver)handleListAdd(w http.ResponseWriter, r *http.Request) {
 	}
 
 	for _, listname := range listnames {
-		c.todolists.AddItems(listname, items...)
+		//only add non-empty strings
+		for _, item := range items{
+			if len(item) > 0{
+				c.todolists.AddItems(listname, item)
+			}
+		}
 	}
-
 	io.WriteString(w, outcomeMessage(true, ""))
 }
 
@@ -223,8 +227,8 @@ func (c *copyserver) handleListSetIndex(w http.ResponseWriter, r *http.Request) 
 }
 
 func (c *copyserver) handleWebAdd(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, fmt.Sprintf(`<html>
-  <form action="http://%s:%d/todo/add">
+	io.WriteString(w, fmt.Sprintf(`<html><a href="getall">Get</a><br><br>
+  <form action="http://%s:%d/todo/web/add_redirect">
     <input type="text" name="item"><br>
     <input type="text" name="item"><br>
     <input type="text" name="item"><br>
@@ -236,12 +240,73 @@ func (c *copyserver) handleWebAdd(w http.ResponseWriter, r *http.Request) {
 `, c.host, c.port))
 }
 
+func (c *copyserver)handleWebAddWithRedirect(w http.ResponseWriter, r *http.Request) {
+	if !handleParseFormData(w, r) {
+		return
+	}
+
+	items := r.Form["item"]
+	if len(items) == 0 {
+		io.WriteString(w, outcomeMessage(false, "no items to add")) //todo display available endpoints
+		return
+	}
+
+	listnames := r.Form["list"]
+	if listnames == nil {
+		listnames = append(listnames, defaultlist)
+	}
+
+	for _, listname := range listnames {
+		//only add non-empty strings
+		for _, item := range items{
+			if len(item) > 0{
+				c.todolists.AddItems(listname, item)
+			}
+		}
+	}
+
+	http.Redirect(w,r,"/todo/web/getall", http.StatusTemporaryRedirect)
+}
+//sam has handleListRemove but with redirect
+func (c *copyserver) handleWebRemoveWithRedirect(w http.ResponseWriter, r *http.Request) {
+	if !handleParseFormData(w, r) {
+		return
+	}
+
+	items := r.Form["index"]
+	if len(items) == 0 {
+		io.WriteString(w, outcomeMessage(false, "no items to remove")) //todo display available endpoints
+		return
+	}
+
+	var indicies []int
+	for _, indexStr := range items {
+		index, err := strconv.Atoi(indexStr)
+		if err != nil {
+			io.WriteString(w, outcomeMessage(false, fmt.Sprintf("Unable to parse index string: %s\n", index)))
+			continue
+		}
+		indicies = append(indicies, index)
+	}
+
+	listnames := r.Form["list"]
+	if listnames == nil {
+		listnames = append(listnames, defaultlist)
+	}
+
+	for _, listname := range listnames {
+		c.todolists.RemoveItems(listname, indicies...)
+	}
+	http.Redirect(w,r,"/todo/web/getall", http.StatusTemporaryRedirect)
+}
+
 func (c *copyserver) handleWebGetAll(w http.ResponseWriter, r *http.Request) {
 	html := "<html>"
+	html += `<a href="add">Add</a><br><br>`
 	for listname, list := range c.todolists {
 		html += fmt.Sprintf("%s<hr><table>",listname )
 		for i, item := range list {
-			rmBtn := fmt.Sprintf(`<form action="http://%s:%d/todo/remove">
+			rmBtn := fmt.Sprintf(`<form action="http://%s:%d/todo/web/remove_redirect">
 			<input type="hidden" name="list" value="%s">
 			<input type="hidden" name="index" value="%d">
 			<input type="submit" value="rm"></form>`, c.host, c.port, listname, i)
@@ -263,14 +328,11 @@ func handleParseFormData(w http.ResponseWriter, r *http.Request) bool {
 }
 
 //todo what is a better way to do this????
-func outcomeMessage(outcome bool, msg string) string {
-	m := make(map[string]string)
+func outcomeMessage(ok bool, msg string) string {
+	m := make(map[string]interface{})
 	if len(msg) != 0 {
 		m["message"] = msg
 	}
-	m["result"] = "failed"
-	if outcome {
-		m["result"] = "success"
-	}
+	m["ok"] = ok
 	return ToJSON(m)
 }
